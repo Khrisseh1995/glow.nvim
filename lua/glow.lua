fca571b6661604cf76f2eb63e52d153aa115eb88
Chris Clark <chris.clark876@gmail.com>
2021-06-15 01:36:03 +0100

    Add basic markdown rendering in a separate window

--- lua/glow.lua
+++ lua/glow.lua
@@ -1,6 +1,7 @@
 local api = vim.api
 local win, buf
 local M = {}
+local markdownWindowId = nil
 
 local function validate(path)
   if vim.fn.executable("glow") == 0 then
@@ -27,7 +28,7 @@ local function validate(path)
 
   local ext = vim.fn.fnamemodify(path, ":e")
   if ext ~= "md" then
-    api.nvim_err_writeln("glow only support markdown files")
+    wpi.nvim_err_writeln("glow only support markdown files")
     return
   end
 
@@ -37,7 +38,7 @@ end
 local function call_go_command()
   local script = [[
     version="1.4.1"
-    os=$(uname -s | tr "[:upper:]" "[:lower:]")
+    s=$(uname -s | tr "[:upper:]" "[:lower:]")
     arch=$(uname -p)
     [ -z "$arch" ] || [ "$arch" == "unknown" ] && arch="x86_64"
     filename="glow_${version}_${os}_${arch}.tar.gz"
@@ -65,6 +66,7 @@ end
 
 function M.create_commands()
   vim.cmd("command! -nargs=? Glow :lua require('glow').glow('<f-args>')")
+  vim.cmd("command! -nargs=? TestBuffer :lua require('glow').create_markdown_window('<f-args>')")
   vim.cmd("command! GlowInstall :lua require('glow').download_glow()")
 end
 
@@ -73,81 +75,101 @@ function M.download_glow()
     api.nvim_err_writeln("golang not installed. Please provide it first")
   end
 
-  if vim.fn.executable("glow") == 1 then
-    local answer = vim.fn.input(
-                     "latest glow already installed, do you want update? Y/n = ")
-    answer = string.lower(answer)
-    while answer ~= "y" and answer ~= "n" do
-      answer = vim.fn.input("please answer Y or n = ")
+    if vim.fn.executable("glow") == 1 then
+      local answer = vim.fn.input(
+                       "latest glow already installed, do you want update? Y/n = ")
       answer = string.lower(answer)
+      while answer ~= "y" and answer ~= "n" do
+        answer = vim.fn.input("please answer Y or n = ")
+        answer = string.lower(answer)
+      end
+
+      if answer == "n" then
+        api.nvim_out_write("\n")
+        return
+      end
+      api.nvim_out_write("updating glow..\n")
+    else
+      print("installing glow..")
     end
+    call_go_command()
+  end
+  -- open_window draws a custom window with the markdown contents
+  local function open_window(path)
 
-    if answer == "n" then
-      api.nvim_out_write("\n")
-      return
+    -- window size
+    local width = api.nvim_get_option("columns")
+    local height = api.nvim_get_option("lines")
+    local win_height = math.ceil(height * 0.8 - 4)
+    local win_width = math.ceil(width * 0.8)
+    local row = math.ceil((height - win_height) / 2 - 1)
+    local col = math.ceil((width - win_width) / 2)
+
+    -- BORDERS
+    local border_buf = api.nvim_create_buf(false, true)
+    local title = vim.fn.fnamemodify(path, ":.")
+    local border_opts = {
+      style = "minimal",
+      relative = "editor",
+      width = win_width + 2,
+      height = win_height + 2,
+      row = row - 1,
+      col = col - 1,
+    }
+    local border_lines = {
+      '┌' .. title .. string.rep('─', win_width - #title) .. '┐',
+    }
+    local middle_line = '│' .. string.rep(' ', win_width) .. '│'
+    for _ = 1, win_height do
+      table.insert(border_lines, middle_line)
     end
-    api.nvim_out_write("updating glow..\n")
-  else
-    print("installing glow..")
+    table.insert(border_lines, '└' .. string.rep('─', win_width) .. '┘')
+    api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
+    api.nvim_open_win(border_buf, true, border_opts)
+
+    local opts = {
+      style = "minimal",
+      relative = "editor",
+      width = win_width,
+      height = win_height,
+      row = row,
+      col = col,
+    }
+
+    -- create preview buffer and set local options
+    buf = api.nvim_create_buf(false, true)
+    win = api.nvim_open_win(buf, true, opts)
+    api.nvim_command("au BufWipeout <buffer> exe 'silent bwipeout! '" .. border_buf)
+    api.nvim_buf_set_keymap(buf, "n", "q", ":lua require('glow').close_window()<cr>",
+                            {noremap = true, silent = true})
+    api.nvim_buf_set_keymap(buf, "n", "<Esc>", ":lua require('glow').close_window()<cr>",
+                            {noremap = true, silent = true})
+
+    -- set local options
+    api.nvim_buf_set_option(buf, "bufhidden", "wipe")
+    api.nvim_win_set_option(win, "winblend", 0)
+    vim.fn.termopen(string.format("glow %s", vim.fn.shellescape(path)))
   end
-  call_go_command()
+
+function M.test()
+  return 'hello'
 end
--- open_window draws a custom window with the markdown contents
-local function open_window(path)
 
-  -- window size
-  local width = api.nvim_get_option("columns")
-  local height = api.nvim_get_option("lines")
-  local win_height = math.ceil(height * 0.8 - 4)
-  local win_width = math.ceil(width * 0.8)
-  local row = math.ceil((height - win_height) / 2 - 1)
-  local col = math.ceil((width - win_width) / 2)
-
-  -- BORDERS
-  local border_buf = api.nvim_create_buf(false, true)
-  local title = vim.fn.fnamemodify(path, ":.")
-  local border_opts = {
-    style = "minimal",
-    relative = "editor",
-    width = win_width + 2,
-    height = win_height + 2,
-    row = row - 1,
-    col = col - 1,
-  }
-  local border_lines = {
-    '┌' .. title .. string.rep('─', win_width - #title) .. '┐',
-  }
-  local middle_line = '│' .. string.rep(' ', win_width) .. '│'
-  for _ = 1, win_height do
-    table.insert(border_lines, middle_line)
-  end
-  table.insert(border_lines, '└' .. string.rep('─', win_width) .. '┘')
-  api.nvim_buf_set_lines(border_buf, 0, -1, false, border_lines)
-  api.nvim_open_win(border_buf, true, border_opts)
-
-  local opts = {
-    style = "minimal",
-    relative = "editor",
-    width = win_width,
-    height = win_height,
-    row = row,
-    col = col,
-  }
-
-  -- create preview buffer and set local options
-  buf = api.nvim_create_buf(false, true)
-  win = api.nvim_open_win(buf, true, opts)
-  api.nvim_command("au BufWipeout <buffer> exe 'silent bwipeout! '" .. border_buf)
-  api.nvim_buf_set_keymap(buf, "n", "q", ":lua require('glow').close_window()<cr>",
-                          {noremap = true, silent = true})
-  api.nvim_buf_set_keymap(buf, "n", "<Esc>", ":lua require('glow').close_window()<cr>",
-                          {noremap = true, silent = true})
-
-  -- set local options
-  api.nvim_buf_set_option(buf, "bufhidden", "wipe")
-  api.nvim_win_set_option(win, "winblend", 0)
-  vim.fn.termopen(string.format("glow %s", vim.fn.shellescape(path)))
+function M.create_markdown_window(file)
+  -- Split window to the right hand side of current window
+  vim.cmd('set splitright')
+  vim.cmd('vsplit')
+  -- Create window and buffer
+  local win = vim.api.nvim_get_current_win()
+  local buf = vim.api.nvim_create_buf(true, true)
+  -- Set buffer to newly created window
+  vim.api.nvim_win_set_buf(win, buf)
+  -- Execute glow command inside new terminal
+  vim.fn.termopen(string.format("glow %s", vim.fn.shellescape('test.md')))
+  -- Switch back to current window
+  vim.cmd('wincmd p')
 end
+
 
 function M.glow(file)
   local current_win = vim.fn.win_getid()
